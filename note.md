ea66df83ac1d8b100129b052e204072f4e4d63c3
ChrisG661 <59037609+ChrisG661@users.noreply.github.com>
2022-11-16 20:53:40 +0800

    Fix distance

--- note.md
+++ note.md
@@ -5,3 +5,7 @@
 - menu matriks: titik pangkal, titik ujung, vektor posisi, panjang vektor, x, y
 - jumlah vektor dan vektor a+b langsung
 - tambah tanda panah
+
+c 77.35
+a 49.97
+b 58.55
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -119,7 +119,9 @@
         let x = end[1] - start[1];
         let y = end[0] - start[0];
         let position = [x, y];
-        let distance = turf.distance(start, end, { units: "meters" });
+        let distance = turf.distance([start[1], start[0]], [end[1], end[0]], {
+          units: "meters",
+        });
         let bearing = turf.bearing(start, end);
         let distanceX = turf.distance([0, 0], [x, 0], { units: "meters" });
         let distanceY = turf.distance([0, 0], [0, y], { units: "meters" });
